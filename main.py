8c4c271e05015e3dd7ea6ae5d6ae8af9c3d9cabf
lovelypatra <lovelypatralp@gmai.com>
2023-02-20 21:56:13 +0530

    project deployment configured

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
 from sensor.pipeline.batch_prediction import start_batch_prediction
+from sensor.pipeline.training_pipeline import start_training_pipeline
+from sensor.exception import SensorException
+import sys
 
 file_path = "/config/workspace/aps_failure_training_set1.csv"
 
 if __name__=="__main__":
      try:
-          start_batch_prediction(input_file_path=file_path)
+          #start_batch_prediction(input_file_path=file_path)
+          start_training_pipeline()
      except Exception as e:
           raise SensorException(e, sys)
